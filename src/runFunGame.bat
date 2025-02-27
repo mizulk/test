66925732e837ef5c55decddc72d64cd68419e4cf
mizulk <1930764560@qq.com>
2025-02-27 16:03:28 +0800

    fix: 规范变量命名

--- src/runFunGame.bat
+++ src/runFunGame.bat
@@ -1,21 +1,21 @@
 @echo off
 
 @rem 读取程序版本
-set /p a=<FunGame.java
+set /p game_version=<FunGame.java
 
 @rem 创建版本日志
 if not exist "FunGame.log" (
-	echo %a%>FunGame.log
+	echo %game_version%>FunGame.log
 	javac -encoding utf-8 FunGame.java
 )
 
 @rem 读取版本日志
-set /p b=<FunGame.log
+set /p log_version=<FunGame.log
 
 @rem 版本是否相同
-if not "%a%"=="%b%" (
+if not "%game_version%"=="%log_version%" (
 	@rem 覆盖版本日志
-	echo %a%>FunGame.log
+	echo %game_version%>FunGame.log
 	javac -encoding utf-8 FunGame.java
 )
 
